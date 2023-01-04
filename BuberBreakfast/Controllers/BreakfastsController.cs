de25d713ea7975da44d0665d6dd5e741726bf4c7
Corbyn Altman <corbyn.altman01@gmail.com>
2023-01-03 20:58:10 -0500

    first commit

---
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,33 @@
+﻿using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("breakfasts")]
+public class BreakfastsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateBreakfast([FromBody] CreateBreakfastRequest request)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast([FromRoute] Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast([FromRoute] Guid id, [FromBody] UpsertBreakfastRequest request)
+    {
+        return Ok(id);
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast([FromRoute] Guid id)
+    {
+        return Ok(id);
+    }
+}
